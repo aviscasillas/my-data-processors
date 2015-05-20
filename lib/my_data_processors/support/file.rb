ca2b67ce54b550788e0b5a4871ed29290948ffef
Albert Viscasillas <aviscasillas@gmail.com>
2015-05-20 11:32:04 +0200

    Refactor file module

--- lib/my_data_processors/support/file.rb
+++ lib/my_data_processors/support/file.rb
@@ -5,17 +5,21 @@ module MyDataProcessors
     module File
       def process_file(filename, &block)
         buffer = []
-
         ::File.foreach(filename).each do |line|
           buffer << line
 
           next if line.length > 3
 
-          args = buffer.join.split("\t").compact
+          block.call(chunk_from_file_line(buffer.join))
           buffer.clear
-
-          block.call(MyDataProcessors::Models::Chunk.new(*args))
         end
+      end
+
+      def chunk_from_file_line(line)
+        args = line.split("\t").compact
+        attrs = { id: args[0], date: args[1], encdata: args[2] }
+
+        MyDataProcessors::Models::Chunk.new(attrs)
       end
     end
   end
